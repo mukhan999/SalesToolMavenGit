3f898bf0ca6a40bcbb36ee124ed38171fd1d7201
user <user@192.168.1.100>
2016-06-11 17:43:33 +0530

    Initial Check in

---
+++ SalesToolMavenGit/src/main/java/SalesTool/SalesToolMavenGit/App.java
@@ -0,0 +1,13 @@
+package SalesTool.SalesToolMavenGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
