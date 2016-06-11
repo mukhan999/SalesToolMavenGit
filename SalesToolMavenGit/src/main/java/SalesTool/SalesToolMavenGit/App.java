96deab357f1a54d31e39e7eb69553f4318ae0517
user <user@192.168.1.100>
2016-06-11 20:51:02 +0530

    changes to App.java and pom.xml files

--- SalesToolMavenGit/src/main/java/SalesTool/SalesToolMavenGit/App.java
+++ SalesToolMavenGit/src/main/java/SalesTool/SalesToolMavenGit/App.java
@@ -1,9 +1,12 @@
 package SalesTool.SalesToolMavenGit;
 
+import org.testng.annotations.Test;
+
 /**
  * Hello world!
  *
  */
+@Test
 public class App 
 {
     public static void main( String[] args )
