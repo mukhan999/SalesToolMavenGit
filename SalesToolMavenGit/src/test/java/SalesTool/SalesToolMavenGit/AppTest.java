02f7b9bfba83f5454ed350edc7d481f3f55a8337
user <user@192.168.1.101>
2016-06-12 15:18:59 +0530

    Added Testng framework notation

--- SalesToolMavenGit/src/test/java/SalesTool/SalesToolMavenGit/AppTest.java
+++ SalesToolMavenGit/src/test/java/SalesTool/SalesToolMavenGit/AppTest.java
@@ -1,38 +1,33 @@
 package SalesTool.SalesToolMavenGit;
 
-import junit.framework.Test;
+//import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
+import org.testng.annotations.Test;
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
-        assertTrue( true );
+    	System.out.println("Hello world program");
+       // AssertTrue( true );
     }
 }
