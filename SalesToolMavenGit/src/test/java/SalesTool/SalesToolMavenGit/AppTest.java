f193ee038f3971407ca341cebc42fb2a1e8e1f48
user <user@192.168.1.101>
2016-06-12 16:22:54 +0530

    changes done to print statement

--- SalesToolMavenGit/src/test/java/SalesTool/SalesToolMavenGit/AppTest.java
+++ SalesToolMavenGit/src/test/java/SalesTool/SalesToolMavenGit/AppTest.java
@@ -27,7 +27,7 @@ public class AppTest
     @Test
     public void testApp()
     {
-    	System.out.println("Hello world program....11111");
+    	System.out.println("Hello world program....11111.....222222");
        // AssertTrue( true );
     }
 }
